0d5af611cbee3b006c1657f739298adfe866c1ee
Arve Knudsen <arve.knudsen@gmail.com>
2014-03-16 11:47:31 +0100

    Use concatenated ChucKJS

--- package.js
+++ package.js
@@ -3,23 +3,9 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.use('require');
-    api.use('underscore-string-latest');
     api.use('q');
+    api.use('underscore-string-latest');
+    api.use('require');
 
     api.add_files('lib/chuck.js', 'client');
-    api.add_files('lib/parser.js', 'client');
-    api.add_files('lib/chuck/audioContextService.js', 'client');
-    api.add_files('lib/chuck/helpers.js', 'client');
-    api.add_files('lib/chuck/instructions.js', 'client');
-    api.add_files('lib/chuck/lexer.js', 'client');
-    api.add_files('lib/chuck/logging.js', 'client');
-    api.add_files('lib/chuck/namespace.js', 'client');
-    api.add_files('lib/chuck/nodes.js', 'client');
-    api.add_files('lib/chuck/parserService.js', 'client');
-    api.add_files('lib/chuck/scanner.js', 'client');
-    api.add_files('lib/chuck/types.js', 'client');
-    api.add_files('lib/chuck/ugen.js', 'client');
-    api.add_files('lib/chuck/vm.js', 'client');
-    api.add_files('lib/chuck/libs/math.js', 'client');
 });
